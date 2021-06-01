6374ce8e2d433fcff3d75b645747bca5852f9ae0
Damar Nur Ichwan <71055434+damar-nur-ichwan@users.noreply.github.com>
2021-06-01 22:54:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,50 @@
-const config = require('./config')
-const client = config.client
+const Influx = require('influx');
 
-let Insert = async(measurement,value)=>{
+module.exports = class InfluxDB {
+  constructor(database,host,port,username,password){
+    this.client = new Influx.InfluxDB({
+      database,
+      host,
+      port: port || 8086,
+      username: username || process.env.UNAME,
+      password: password || process.env.PASSWORD,
+    })
+  }
+
+  Insert = async(measurement,value)=>{
     try {
       const rows = [{
-          measurement,
-          fields: {value},
-        }];
-      await client.writePoints(rows);
+        measurement,
+        fields: {value},
+      }];
+      await this.client.writePoints(rows);
       console.log('Data stored successfully!');
     } catch (err) {
       console.log(`Error while processing ${err}`);
     }
   };
-  
-  let Select = async (param,measurement,column,where) => {
-      if(Param === 'all'){param===''}
-      where = `where ${where}` || ''
-    try{
-        const results = await client.query(
-            `select ${param}(${column}) from ${measurement} ${where}`);
-        return results[0]
-    } catch(err){
-        console.log('err while processing $(err)')
-    }
-  };
 
- let Delete = async (measurement,where) => {
-      where = `where ${where}` || ''
+  Select = async (param,measurement,column,where) => {
+    if(Param === 'all'){param===''}
+    where = `where ${where}` || ''
     try{
-        const results = await client.query(
-            `select from ${measurement} ${where}`);
+      const results = await this.client.query(
+        `select ${param}(${column}) from ${measurement} ${where}`);
         return results[0]
-    } catch(err){
+      } catch(err){
         console.log('err while processing $(err)')
+      }
     }
-  };
-module.exports={Insert, Select, Delete}
+    
+    Delete = async (measurement,where) => {
+      where = `where ${where}` || ''
+      try{
+        const results = await this.client.query(
+          `select from ${measurement} ${where}`);
+          return results[0]
+        } catch(err){
+          console.log('err while processing $(err)')
+        }
+      }
+    }
+
